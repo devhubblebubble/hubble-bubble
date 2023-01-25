406339f11c2ac214a7fa30910ec2ea3229fca6d3
Arun Paul <arun@stackmod.io>
2023-01-25 16:29:35 +0530

    fix: bug

--- resources/views/scripts/home-scripts.blade.php
+++ resources/views/scripts/home-scripts.blade.php
@@ -837,6 +837,7 @@
                         required: true
                     },
                     email_address: {
+                        email:true,
                         required: true
                     },
                     contact_preffered_country: {
@@ -857,6 +858,7 @@
                         required: "Please enter your phone number."
                     },
                     email_address: {
+                        email: "Please enter a valid email ID.",
                         required: "Please enter your email ID."
                     },
                     contact_preffered_country: {
