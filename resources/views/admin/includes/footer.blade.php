b00659957369a9c6a8acb5a98381933f5ccf193a
Arun Paul <arun@stackmod.io>
2023-01-25 10:08:57 +0530

    feat: admin panel completed

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -6,7 +6,6 @@
             © <script>
                 document.write(new Date().getFullYear())
             </script>,
-            made with <i class="fa fa-heart"></i> by
             <a href="#" class="font-weight-bold" target="_blank">Hubble Bubble</a>
             for a better future.
             </div>
