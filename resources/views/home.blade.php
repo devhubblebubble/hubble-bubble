d9e4c0aeb9d13d7629a32c0a9ab051cca88c8597
Shijin Gireesan <shijingireesan@Shijins-MacBook-Pro.local>
2023-02-02 22:51:22 +0530

    safari zoom issue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -115,7 +115,7 @@
         <!--/. Our Big Bang -->
 
         <!-- Find Us -->
-        <!-- @include('templates.modals.find-us') -->
+        @include('templates.modals.find-us')
         <!--/. Find Us -->
         
         <!-- Student Volunteers -->
@@ -127,7 +127,7 @@
         <!--/. Story of Jevin Zac -->
 
         <!-- Contact Us -->
-        <!-- @include('templates.modals.contact-us') -->
+        @include('templates.modals.contact-us')
         <!--/. Contact Us -->
         
         <!-- Confirmation -->
