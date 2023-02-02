2b46464aca2d5d7bf1f3bc8b70e34ced4016300c
Shijin Gireesan <shijingireesan@Shijins-MacBook-Pro.local>
2023-02-02 22:55:29 +0530

    safari zoom issue

--- resources/views/layouts/home-layout.blade.php
+++ resources/views/layouts/home-layout.blade.php
@@ -4,7 +4,6 @@
 <head>
 <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/> <!--320-->
     <meta name=“ theme-color” content=“#151515>
     <title>Hubble Bubble</title>
 
