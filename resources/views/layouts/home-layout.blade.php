f05c0bf83f87690e7e33cd9baab700fdcabfe27c
Shijin Gireesan <shijingireesan@Shijins-MacBook-Pro.local>
2024-02-16 20:22:15 +0530

    tag manager code insert

--- resources/views/layouts/home-layout.blade.php
+++ resources/views/layouts/home-layout.blade.php
@@ -33,6 +33,11 @@
 </head>
 
 <body class="loading">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9GSX9J6"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     @yield('content')
     <!-- Core JS Files -->
     <!-- jQuery -->
